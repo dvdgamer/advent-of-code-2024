aa6e9d43c18ca7c7c08328b1123fc3b05a559b31
dvdgamer <davidnmponte@gmail.com>
2024-12-02 18:31:45 +0100

    Refactor problem.js to use hardcoded input and implement counting logic for safe arrays

--- day2/problem.js
+++ day2/problem.js
@@ -1,14 +1,38 @@
-import { input } from "./input.js";
+// import { input } from "./input.js";
 
+const input = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
 const inputArray = input.split("\n")
+
+
+let increasingArrays = [];
+let decreasingArrays = [];
+let safeArray = [];
 
 // loops through array
 for (let i = 0; i < inputArray.length; i++) {
-  const element = array[i];
+  const element = inputArray[i].split(" ");
+
   // loops through element
-  for (let h = 0; h < element.length; h++) {
-    const element = array[h];
+  let count = 0;
+  for (let j = 0; j < element.length; j++) {
+    const curr = element[j];
+    const next = element[j + 1];
+
+    if (curr + 3 <= next && next >= curr + 1) {
+      count++
+    } else if (curr - 3 <= next && next <= curr - 1) {
+      count--
+    }
+  }
+  console.log("count", count)
+  if (Math.abs(count) === element.length - 1) {
+    safeArray.push(element)
   }
 }
 
-console.log(inputArray)
+console.log(safeArray)
